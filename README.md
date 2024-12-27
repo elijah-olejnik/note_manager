13a3881a8064164858f0b839b8697ad05f780a8d
Elijah Olejnik <elijah.olejnik@gmail.com>
2024-12-27 14:58:31 +0700

    2024-12-27

--- README.md
+++ README.md
@@ -12,8 +12,8 @@
     5. Date of creation
     6. Issue date
 #### date_changer.py
-    This script asks user to input the values for the note, saves the values
-    to the class attributes and then prints them out formatted to the console,
+    This script asks user to input the values for the note (as it was indicated in the example attached to the task),
+    saves the values to the class attributes and then prints them out formatted to the console,
     dates are printed without year.
 
     So, this piece of code contain:
@@ -23,9 +23,13 @@
     3. console user input function with error handling logic
 #### add_input.py
     This script adds some performance and code optimisations and 
-    the list for saving 3 titles
+    the list for saving 3 titles (as it was indicated in the example attached to the task)
 #### add_list.py
-    In process...
+    I thought that it will be more convenient to add titles (as many as you want) right in your note text while typing
+    it in console. So I added this feature and also made my code more readable with several optimisations: 
+
+    1. I added the match-case statement in the fill_from_console() function instead of complicated if-else conditions
+    2. I separated the date input checkings to the function is_date_accepted()
 #### final
-    The script still asks user to input the values for the note but saves 
+    The script still asks user to input the values for the note but (as a new feature) saves 
     the values to the dictionary and then prints it out formatted to the console
--- add_list.py
+++ add_list.py
@@ -11,58 +11,65 @@ class Note:
 
     def __init__(self):
         self.username = None
-        self.titles = ["", "", ""] # TODO: I need to refactor my code for multiple title input
+        self.titles = []
         self.content = None
         self.status = None
         self.created_date = None
         self.issue_date = "Termless"
 
+    def is_date_accepted(self, str_date = "", is_issue_date = False):
+        try:
+            date = datetime.strptime(str_date, self.in_date_fmt)
+            if is_issue_date:
+                if date < self.created_date:
+                    raise ValueError("The expiration date of a note can't be "
+                                     "earlier than the date it was created")
+                self.issue_date = date
+                return True
+            else:
+                if date.date() != datetime.today().date():
+                    print("\nWhat are you hiding, cheater? =)")
+                self.created_date = date
+                return True
+        except ValueError as e:
+            print(f"\n{e}")
+            return False
+
     def fill_from_console(self):
-        prompt_list = ('Enter your name', 'Enter a title',
-                       'Enter a second title or press Enter to skip',
-                       'Enter a third title or press Enter to skip',
-                       'Enter the note text', 'Enter the note state: "a" if Active or "c" if Completed',
+        prompt_list = ('Enter your name', 'Enter your note. If you want to add a title, or additional '
+                       'titles/headers within your text, use double tilde, e.g. ~~Some title~~',
+                       'Enter the note state: "a" if Active or "c" if Completed',
                        'Enter the date of note creation in "dd-mm-yyyy" format or press Enter for auto input',
                        'Enter the note expiration date in "dd-mm-yyyy" format or press Enter if termless')
-        input_list = []
         for i, prompt in enumerate(prompt_list):
             while True:
                 user_input = input("\n"
                                    + prompt
                                    + (f" (today is {datetime.strftime(datetime.today(), self.in_date_fmt)}): "
-                                   if i == 6 else ": "))
-                if (i not in (2, 3, 6, 7) and not user_input) or (i == 5 and user_input not in ('c', 'a')):
+                                   if i == 3 else ": "))
+                if i < 3 and not user_input:
                     continue
-                if i > 4:
-                    if i == 5:
-                        self.status = Status.ACTIVE if user_input == 'a' else Status.COMPLETED
-                        break
-                    if not user_input:
-                        if i == 6:
-                            self.created_date = datetime.now()
-                        break
-                    try:
-                        date = datetime.strptime(user_input, self.in_date_fmt)
-                        if i == 6:
-                            if date.date() != datetime.today().date():
-                                print ("\nWhat are you hiding, cheater? =)")
-                            self.created_date = date
+                match i:
+                    case 0:
+                        self.username = user_input
+                    case 1:
+                        self.titles = user_input.split("~~")[1::2]
+                        self.content = user_input.replace("~~", "\n\n")
+                    case 2:
+                        if user_input == 'a':
+                            self.status = Status.ACTIVE
+                        elif user_input == 'c':
+                            self.status = Status.COMPLETED
+                        else:
+                            continue
+                    case _:
+                        if not user_input:
+                            if i == 3:
+                                self.created_date = datetime.now()
                             break
-                        if i == 7:
-                            if date < self.created_date:
-                                raise ValueError("The expiration date of a note can't be "
-                                                 "earlier than the date it was created")
-                            self.issue_date = date
-                            break
-                    except ValueError as e:
-                        print(f"\n{e}")
-                        continue
-                input_list.append(user_input)
+                        if not self.is_date_accepted(user_input, i == 4):
+                            continue
                 break
-        self.username = input_list[0]
-        for i in range(3):
-            self.titles[i] = input_list[i + 1]
-        self.content = input_list[4]
 
     def show_to_console(self):
         print("\nYour note is saved. You've entered:\n")
