430f76d54300c6e2c2673e93fced7dce0f50bd30
Elijah Olejnik <elijah.olejnik@gmail.com>
2024-12-29 01:09:58 +0700

    2024-29-12

--- final_dict.py
+++ final_dict.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from enum import Enum
+import re
 
 class Status(Enum):
     ACTIVE = 0
@@ -7,12 +8,12 @@ class Status(Enum):
     TERMLESS = 2
 
 class Note:
-    in_date_fmt = "%d-%m-%Y"
-    out_date_fmt = "%A %d %B"
-    title_marker = "~~"
+    _in_date_fmt = "%d-%m-%Y"
+    _out_date_fmt = "%A %d %B"
+    _regexp = r'\{\{(.*?)\}\}'
 
     def __init__(self):
-        self.note = {
+        self._note = {
             "username" : "",
             "titles" : [],
             "content" : "",
@@ -22,26 +23,26 @@ class Note:
         }
 
     def get_note(self):
-        return self.note
+        return self._note
 
     def set_note(self, note=None):
         if note is None:
             note = {}
-        self.note = note
+        self._note = note
 
     def _is_date_accepted(self, str_date = "", is_issue_date = False):
         try:
-            date = datetime.strptime(str_date, self.in_date_fmt)
+            date = datetime.strptime(str_date, self._in_date_fmt)
             if is_issue_date:
-                if date < self.note["created_date"]:
+                if date < self._note["created_date"]:
                     raise ValueError("The expiration date of a note can't be "
                                      "earlier than the date it was created")
-                self.note["issue_date"] = date
+                self._note["issue_date"] = date
                 return True
             else:
                 if date.date() != datetime.today().date():
                     print("\nWhat are you hiding, cheater? =)")
-                self.note["created_date"] = date
+                self._note["created_date"] = date
                 return True
         except ValueError as e:
             print(f"\n{e}")
@@ -50,7 +51,7 @@ class Note:
     def fill_from_console(self):
         prompts = (
             'Enter your name', 'Enter your note. If you want to add a title,\nor an additional '
-                               'titles/headers within your text,\nuse double tilde, e.g. ~~Some title~~',
+                               'titles/headers within your text,\nuse double curly braces, e.g. {{Some title}}',
             'Enter the note state: type "a" if ACTIVE, "c" if COMPLETED or skip pressing Enter if TERMLESS',
             'Enter the date of note creation in "dd-mm-yyyy" format or press Enter for auto input',
             'Enter the note expiration date in "dd-mm-yyyy" format or press Enter if termless'
@@ -59,34 +60,38 @@ class Note:
             while True:
                 user_input = input("\n"
                                    + prompt
-                                   + (f" (today is {datetime.strftime(datetime.today(), self.in_date_fmt)}): "
+                                   + (f" (today is {datetime.strftime(datetime.today(), self._in_date_fmt)}): "
                                    if i == 3 else ": "))
                 if i < 2 and not user_input:
                     continue
                 match i:
                     case 0:
-                        self.note["username"] = user_input
+                        self._note["username"] = user_input
                     case 1:
-                        if user_input.count(self.title_marker) > 1:
-                            self.note["titles"] = user_input.split(self.title_marker)[1::2]
-                            self.note["content"] = user_input.replace(self.title_marker, "\n\n")
-                            break
-                        self.note["content"] = user_input
-                        if user_input.count(" ") > 0:
-                            self.note["titles"].append(user_input.split(" ")[0] + " " + user_input.split(" ")[1])
+                        titles = re.findall(self._regexp, user_input)
+                        if titles:
+                            self._note["titles"] = titles
+                            self._note["content"] = re.sub(self._regexp, r'\n\n\1\n\n', user_input)
                         else:
-                            self.note["titles"].append(user_input)
+                            self._note["content"] = user_input
+                            words = user_input.split()
+                            if len(words) == 1:
+                                self._note["titles"].append(words[0])
+                            elif len(words) == 2:
+                                self._note["titles"].append(" ".join(words[:2]))
+                            else:
+                                self._note["titles"].append(" ".join(words[:3]))
                     case 2:
                         if user_input == 'a':
-                            self.note["status"] = Status.ACTIVE
+                            self._note["status"] = Status.ACTIVE
                         elif user_input == 'c':
-                            self.note["status"] = Status.COMPLETED
+                            self._note["status"] = Status.COMPLETED
                         else:
-                            self.note["status"] = Status.TERMLESS
+                            self._note["status"] = Status.TERMLESS
                     case _:
                         if not user_input:
                             if i == 3:
-                                self.note["created_date"] = datetime.now()
+                                self._note["created_date"] = datetime.now()
                             break
                         if not self._is_date_accepted(user_input, i == 4):
                             continue
@@ -98,21 +103,21 @@ class Note:
 
     def __str__(self):
         output_string = ""
-        for key, value in self.note.items():
+        for key, value in self._note.items():
             match key:
                 case "titles":
                     output_string += "\n".join(
                         key.capitalize()[:-1] + " " + str(x + 1) + ": " + str(t) for x, t in enumerate(value)
                     ) + "\n"
                 case "content":
-                    "\n" + key.capitalize() + "\n\n" + value + "\n\n"
+                    output_string += "\n" + key.capitalize() + ":" + "\n" + value + "\n\n"
                 case "status":
                     output_string += key.capitalize() + ": " + value.name + "\n"
                 case "created_date":
-                    output_string += key.capitalize() + ": " + datetime.strftime(value, self.out_date_fmt) + "\n"
+                    output_string += key.capitalize() + ": " + datetime.strftime(value, self._out_date_fmt) + "\n"
                 case "issue_date":
                     output_string += key.capitalize() + ": " + (
-                        datetime.strftime(value, self.out_date_fmt)
+                        datetime.strftime(value, self._out_date_fmt)
                         if isinstance(value, datetime) else value
                     )
                 case _:
