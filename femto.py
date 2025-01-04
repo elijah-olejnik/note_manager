9baab65a102d1fb2b694b889396e760b75fa4e51
elijah-olejnik <elijah.olejnik@gmail.com>
2025-01-05 02:12:08 +0700

    2025-01-05

---
+++ femto.py
@@ -0,0 +1,56 @@
+import curses
+
+def femto(stdscr, initial_text = ""):
+    stdscr.clear()
+    cursor_x = 0
+    cursor_y = 0
+    text = initial_text.split('\n') if initial_text else [""]
+    while True:
+        stdscr.clear()
+        for i, line in enumerate(text):
+            stdscr.addstr(i, 0, line)
+        stdscr.move(cursor_y, cursor_x)
+        stdscr.refresh()
+        key = stdscr.getch()
+        match key:
+            case curses.KEY_UP:
+                cursor_y = max(0, cursor_y - 1)
+            case curses.KEY_DOWN:
+                cursor_y = min(len(text) - 1, cursor_y + 1)
+            case curses.KEY_LEFT:
+                cursor_x = max(0, cursor_x - 1)
+            case curses.KEY_RIGHT:
+                cursor_x = min(len(text[cursor_y]), cursor_x + 1)
+            case 10 | 13:  # Enter key
+                text.insert(cursor_y + 1, "")
+                cursor_y += 1
+                cursor_x = 0
+            case 127:  # Backspace key
+                if cursor_x > 0:
+                    text[cursor_y] = text[cursor_y][:cursor_x - 1] + text[cursor_y][cursor_x:]
+                    cursor_x -= 1
+                elif cursor_y > 0:
+                    cursor_x = len(text[cursor_y - 1])
+                    text[cursor_y - 1] += text[cursor_y]
+                    text.pop(cursor_y)
+                    cursor_y -= 1
+            case 27:  # ESC key to exit
+                break
+            case _:
+                if cursor_y >= len(text):
+                    text.append("")
+                text[cursor_y] = text[cursor_y][:cursor_x] + chr(key) + text[cursor_y][cursor_x:]
+                cursor_x += 1
+        # Ensure cursor is within bounds
+        cursor_x = min(len(text[cursor_y]), cursor_x)
+    return '\n'.join(text)
+
+# My sandbox:
+# def main():
+#     initial_text = "This is a sample string.\nFeel free to edit it!"
+#     edited_text = curses.wrapper(femto, initial_text)
+#     print("Edited text:")
+#     print(edited_text)
+#
+# if __name__ == "__main__":
+#     main()
