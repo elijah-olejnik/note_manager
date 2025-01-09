317ae81ff823de88cddf290b16e4930f6f8b11d6
Elijah Olejnik <elijah.olejnik@gmail.com>
2025-01-09 15:41:03 +0700

    2025-01-09

--- femto.py
+++ femto.py
@@ -1,9 +1,9 @@
 # This is my tiny console text editor
+# To use it on windows you should install windows-curses module
 import curses
 
 
-# IDK why I'm getting a symbol in the screen beginning
-def femto(screen, initial_text =""):
+def femto(screen, initial_text=""):
     header = (
         "Edit your note text. To exit and save text press Esc",
         "To add a title surround it with {{double curly braces}}"
@@ -13,6 +13,10 @@ def femto(screen, initial_text =""):
     cursor_x = 0
     cursor_y = 0
     text = initial_text.split('\n') if initial_text else [""]
+    # Define how many lines a page up/down moves
+    page_size = curses.LINES - header_height - 1
+    # Insert mode flag
+    insert_mode = True
     while True:
         screen.clear()
         for i, line in enumerate(header):
@@ -31,11 +35,25 @@ def femto(screen, initial_text =""):
                 cursor_x = max(0, cursor_x - 1)
             case curses.KEY_RIGHT:
                 cursor_x = min(len(text[cursor_y]), cursor_x + 1)
+            case curses.KEY_HOME:
+                cursor_x = 0
+            case curses.KEY_END:
+                cursor_x = len(text[cursor_y])
+            case curses.KEY_DC:  # Del key
+                if cursor_x < len(text[cursor_y]):
+                    text[cursor_y] = text[cursor_y][:cursor_x] + text[cursor_y][cursor_x + 1:]
+                elif cursor_y < len(text) - 1:
+                    text[cursor_y] += text[cursor_y + 1]
+                    text.pop(cursor_y + 1)
+            case curses.KEY_PPAGE:  # Page Up key
+                cursor_y = max(0, cursor_y - page_size)
+            case curses.KEY_NPAGE:  # Page Down key
+                cursor_y = min(len(text) - 1, cursor_y + page_size)
             case 10 | 13:  # Enter key
                 text.insert(cursor_y + 1, "")
                 cursor_y += 1
                 cursor_x = 0
-            case 127:  # Backspace key
+            case 8 | 127:  # Backspace key
                 if cursor_x > 0:
                     text[cursor_y] = text[cursor_y][:cursor_x - 1] + text[cursor_y][cursor_x:]
                     cursor_x -= 1
@@ -46,10 +64,20 @@ def femto(screen, initial_text =""):
                     cursor_y -= 1
             case 27:  # ESC key to exit
                 break
+            case 9:  # Tab key
+                tab_spaces = "    "  # or simply "\t" for a tab character
+                text[cursor_y] = text[cursor_y][:cursor_x] + tab_spaces + text[cursor_y][cursor_x:]
+                cursor_x += len(tab_spaces)
+            case curses.KEY_IC:  # Insert key
+                insert_mode = not insert_mode
             case _:
                 if cursor_y >= len(text):
                     text.append("")
-                text[cursor_y] = text[cursor_y][:cursor_x] + chr(key) + text[cursor_y][cursor_x:]
+                if insert_mode or cursor_x >= len(text[cursor_y]):
+
+                    text[cursor_y] = text[cursor_y][:cursor_x] + chr(key) + text[cursor_y][cursor_x:]
+                else:
+                    text[cursor_y] = text[cursor_y][:cursor_x] + chr(key) + text[cursor_y][cursor_x + 1:]
                 cursor_x += 1
         # Ensure cursor is within bounds
         cursor_x = min(len(text[cursor_y]), cursor_x)
