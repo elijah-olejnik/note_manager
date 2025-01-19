c6d2075437ea091c1f46c7da834770466b78799a
Elijah Olejnik <elijah.olejnik@gmail.com>
2025-01-20 00:35:54 +0700

    2025-01-19

--- data/note.py
+++ data/note.py
@@ -1,13 +1,14 @@
+from utils.enums import NoteStatus
 from dataclasses import dataclass
 from datetime import datetime
-from utils.enums import NoteStatus
+from uuid import UUID
 
 @dataclass
 class Note:
     content: str
-    created_date: datetime.date
-    id_: str
-    issue_date: datetime.date
+    created_date: datetime
+    id_: UUID
+    issue_date: datetime
     status: NoteStatus
     title: str
     username: str
