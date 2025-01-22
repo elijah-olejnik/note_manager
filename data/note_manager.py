75330c88efabb480b044a833a7ba860905866513
Elijah Olejnik <elijah.olejnik@gmail.com>
2025-01-22 18:02:39 +0700

    2025-01-22

--- data/note_manager.py
+++ data/note_manager.py
@@ -57,11 +57,13 @@ class NoteManager:
 
     @property
     def notes(self):
-        """Gives access to the private list of Note objects stored in class."""
+        """The attribute gives access to the private list of Note objects stored in class."""
         return self._notes
 
     def _get_note_index_by_id(self, id_):
-        """Returns an index of the note list element if note.id_ and the given id_ are equal."""
+        """The function return an index of the note list element if note.id_ and the given id_ are equal.
+        If no match found the function return -1.
+        """
         idx = -1
         for i, note in enumerate(self._notes):
             if note.id_ == id_:
@@ -69,6 +71,12 @@ class NoteManager:
         return idx
 
     def _get_notes_indexes_by_filter(self, keys=None, status=None):
+        """The function return a set of unique indexes filtered by keywords and/or status.
+
+        The argument 'keys' takes a list of keywords.
+
+        The argument 'status' takes a NoteStatus(Enum) value.
+        """
         if not self._notes:
             return []
         filtered_indexes = set()
@@ -90,6 +98,10 @@ class NoteManager:
         return filtered_indexes
 
     def import_notes_from_dicts(self, note_dicts):
+        """The function takes a list of dictionaries,
+        loaded from JSON or YAML file and appends it to the _notes list as notes.
+        Raises an exceptions if list is empty or if the dictionaries contain wrong data.
+        """
         if not note_dicts:
             raise ValueError("Failed to import notes from dicts: it's empty.")
         for d in note_dicts:
@@ -99,9 +111,17 @@ class NoteManager:
                 raise DataIntegrityError(f"Failed to import notes from dicts: {e}")
 
     def export_notes_as_dicts(self):
-        return [asdict(note) for note in self._notes] if self._notes else []
+        """The function return a list of dictionaries converted from _notes for serialization purposes
+        if the _notes list is not empty.
+        """
+        if self._notes:
+            return [asdict(note) for note in self._notes] if self._notes else []
 
     def append_note(self, note):
+        """The function takes a created note as an argument and appends it to the _notes list.
+        Also, it appends a note to the file storage and raises a FileIOError exception if
+        export to the file fails.
+        """
         self._notes.append(note)
         try:
             export_to_yaml([asdict(note)], self.storage_path, False)
@@ -109,24 +129,36 @@ class NoteManager:
             warnings.warn(e) # noqa
 
     def get_note_by_id(self, id_):
+        """The function return a note by a given ID or
+        raises a ValueError exception if id_ match not found.
+        """
         for note in self._notes:
             if id_ == note.id_:
                 return note
         raise ValueError(f"Note with ID {id_} not found.")
 
     def save_notes_to_file(self):
+        """The function dumps _notes list to the file storage or raises an exception
+        if export to the file failed.
+        """
         try:
             export_to_yaml(self.export_notes_as_dicts(), self.storage_path)
         except (ValueError, FileIOError) as e:
             warnings.warn(e)
 
     def save_notes_json(self):
+        """The function dumps _notes list to the file storage in JSON format or raises an exception
+        if export to the file failed.
+        """
         try:
             export_to_json(self.export_notes_as_dicts(), self.storage_path)
         except (ValueError, FileIOError) as e:
             warnings.warn(e)
 
     def load_notes_from_file(self):
+        """The function load notes from the file storage or raises an exception when
+        file IO or converting data to Note dataclass object fails.
+        """
         if not self.storage_path.is_file():
             warnings.warn(f"File {self.storage_path} not found.")
             try:
@@ -144,16 +176,28 @@ class NoteManager:
                 warnings.warn(e)
 
     def filter_notes(self, keys=None, state=None):
+        """The function return a list of notes filtered by keywords and/or status.
+
+        The argument 'keys' takes a list of keywords.
+
+        The argument 'state' takes a NoteStatus(Enum) value.
+        """
         found_indexes = self._get_notes_indexes_by_filter(keys, state)
         return [self._notes[i] for i in found_indexes] if found_indexes else []
 
     def delete_note_by_id(self, id_):
+        """The function return a note popped from the _notes list by a given ID
+        or raises a ValueError exception if no id_ match is found.
+        """
         i = self._get_note_index_by_id(id_)
         if i == -1:
             raise ValueError(f"Note with ID {id_} not found.")
         return self._notes.pop(i)
 
     def delete_by_state(self, state):
+        """The function delete _notes filtered by state.
+        It takes a NoteStatus(Enum) value as an argument.
+        """
         found_indexes = self._get_notes_indexes_by_filter(status=state)
         if not found_indexes:
             return False
@@ -162,6 +206,21 @@ class NoteManager:
         return True
 
     def get_urgent_notes_sorted(self):
+        """The function return a list of 3 lists of notes filtered by the deadline.
+        Each note list in the list represents three levels of urgency:
+
+        1. The list with the index 0 represents the notes with the missed deadline sorted by date ascending:
+        the older - the closer to the beginning of the list.
+
+        2. The list with the index 1 represents the notes with the deadline date which match the date
+        the function is called.
+
+        3. The list with the index 3 represents the notes with the deadline date which match the date
+        the function is called + 1 day.
+
+        If no notes match the filter condition of the corresponding urgency level the list will be empty (falsy).
+        So, if no urgent notes at all – the function will return a list of 3 empty (falsy) lists.
+        """
         if not self._notes:
             return None
         missed_dl = []
