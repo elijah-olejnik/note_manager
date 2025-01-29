8d8764facc4c1fc5d848c50ff605b2d7fdeb198b
Elijah Olejnik <elijah.olejnik@gmail.com>
2025-01-30 03:25:04 +0700

    2025-01-29

--- database/notes_operations.py
+++ database/notes_operations.py
@@ -3,116 +3,83 @@ from datetime import datetime
 from utils import NoteStatus
 
 
+def notes_from_data(rows):
+    return [
+        {
+            'id_': row[0],
+            'username': row[1],
+            'title': row[2],
+            'content': row[3],
+            'status': NoteStatus[row[4]],
+            'created_date': datetime.strptime(row[5], "%d-%m-%Y").date(),
+            'issue_date': datetime.strptime(row[6], "%d-%m-%Y").date()
+        } for row in rows
+    ]
+
+
+def data_from_note(note, note_id = None):
+    is_add = len(note) == 6
+    data = [note['title'], note['content'], note['status']]
+    if is_add:
+        data.insert(0, note['username'])
+        data.append(note['created_date'])
+    data.append(note['issue_date'])
+    if not is_add:
+        data.append(note_id)
+    return data
+
+
 def save_note_to_db(note, db_path):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    insert_sql = """
-    INSERT INTO notes (username, title, content, status, created_date, issue_date)
-    VALUES (?, ?, ?, ?, ?, ?)
-    """
-    data = (
-        note['username'],
-        note['title'],
-        note['content'],
-        note['status'].name,
-        datetime.strftime(note['created_date'], "%d-%m-%Y"),
-        datetime.strftime(note['issue_date'], "%d-%m-%Y")
-    )
-    cursor.execute(insert_sql, data)
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT INTO notes (username, title, content, status, created_date, issue_date)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """, data_from_note(note))
+        conn.commit()
 
 
 def load_notes_from_db(db_path):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM notes;")
-    rows = cursor.fetchall()
-    if not rows:
-        return []
-    notes = []
-    for row in rows:
-        notes.append(
-            {
-                'id_' : row[0],
-                'username' : row[1],
-                'title' : row[2],
-                'content' : row[3],
-                'status' : NoteStatus[row[4]],
-                'created_date' : datetime.strptime(row[5], "%d-%m-%Y").date(),
-                'issue_date' : datetime.strptime(row[6], "%d-%m-%Y").date()
-            }
-        )
-    conn.close()
-    return notes
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM notes;")
+        rows = cursor.fetchall()
+    return notes_from_data(rows) if rows else rows
+
 
 def update_note_in_db(note_id, updates, db_path):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    update_sql = """
-    UPDATE notes
-    SET title = ?, content = ?, status = ?, issue_date = ?
-    WHERE id = ?;
-    """
-    data = (
-        updates['title'],
-        updates['content'],
-        updates['status'].name,
-        datetime.strftime(updates['issue_date'], "%d-%m-%Y"),
-        note_id
-    )
-    cursor.execute(update_sql, data)
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            """UPDATE notes SET title = ?, content = ?, status = ?, issue_date = ? WHERE id = ?;""",
+        data_from_note(updates, note_id))
+        conn.commit()
 
 
 def delete_note_from_db(note_id, db_path):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM notes WHERE id = ?;", (note_id,))
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM notes WHERE id = ?;", (note_id,))
+        conn.commit()
 
 
 def search_notes_by_keyword(key, db_path):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute(
-        """SELECT * FROM notes WHERE title LIKE ? OR content LIKE ?;""",
-        (f"%{key}%", f"%{key}%")
-    )
-    rows = cursor.fetchall()
-    conn.close()
-    return [
-        {
-            'id_': row[0],
-            'username': row[1],
-            'title': row[2],
-            'content': row[3],
-            'status': NoteStatus[row[4]],
-            'created_date': datetime.strptime(row[5], "%d-%m-%Y").date(),
-            'issue_date' : datetime.strptime(row[6], "%d-%m-%Y").date()
-        } for row in rows
-    ]
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            """SELECT * FROM notes WHERE title LIKE ? OR content LIKE ?;""",
+            (f"%{key}%", f"%{key}%")
+        )
+        rows = cursor.fetchall()
+    return notes_from_data(rows) if rows else rows
 
 
 def filter_notes_by_status(status, db_path):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute(
-        """SELECT * FROM notes WHERE status = ?;""",
-        (status.name,)
-    )
-    rows = cursor.fetchall()
-    conn.close()
-    return [
-        {
-            'id_': row[0],
-            'username': row[1],
-            'title': row[2],
-            'content': row[3],
-            'status': NoteStatus[row[4]],
-            'created_date': datetime.strptime(row[5], "%d-%m-%Y").date(),
-            'issue_date' : datetime.strptime(row[6], "%d-%m-%Y").date()
-        } for row in rows
-    ]
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            """SELECT * FROM notes WHERE status = ?;""",
+            (status.name,)
+        )
+        rows = cursor.fetchall()
+    return notes_from_data(rows) if rows else rows
